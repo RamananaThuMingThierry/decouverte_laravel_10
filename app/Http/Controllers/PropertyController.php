22d3d7975626c827cfc29b36ed0634b017b9bf28
RAMANANA Thu Ming Thierry <ramananathumingthierry@gmail.com>
2024-06-04 15:14:31 +0300

    Nous avons terminer la partie recherche des biens

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -2,18 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use View;
 use App\Models\Property;
 use Illuminate\Http\Request;
-use View;
+use App\Http\Requests\SearchPropertyRequest;
+use Redirect;
 
 class PropertyController extends Controller
 {
-    public function index(){
-        $properties = Property::paginate();
-        return View('property.index', compact('properties'));
+    public function index(SearchPropertyRequest $request) {
+        $query = Property::query();
+    
+        $validated = $request->validated();
+    
+        if(isset($validated['price'])) {
+            $query->where('price', '<=', $validated['price']);
+        }
+        
+        if(isset($validated['surface'])) {
+            $query->where('surface', '>=', $validated['surface']);
+        }
+    
+        if(isset($validated['rooms'])) {
+            $query->where('rooms', '>=', $validated['rooms']);
+        }
+    
+        if(isset($validated['title'])) {
+            $query->where('title', 'like', "%{$validated['title']}%");
+        }
+    
+        return view('property.index', [
+            'properties' => $query->paginate(6),
+            'input' => $validated
+        ]);
     }
+    
 
-    public function show(){
+    public function show(string $slug, Property $property){
+        $expectedSlug = $property->getSlug();
 
+        if($slug !== $expectedSlug){
+            return Redirect()->route('property.show', ['slug', $expectedSlug, 'property' => $property]);
+        }
+
+        return view('property.show', [
+            'propery' => $property
+        ]);
     }
 }
--- resources/views/property/index.blade.php
+++ resources/views/property/index.blade.php
@@ -5,20 +5,31 @@
 @section('contenu')
     <div class="bg-light p-5 mb-5 text-center">
       <form action="" method="get" class="container d-flex gap-2">
-        @include('shared.input', ['type' => 'number', 'placeholder' => 'Budget Max', 'name' => 'price', 'valeur' => ])
+        <input class="form-control" type="number" placeholder="Surface minimum" name="surface" value="{{ $input['surface'] ?? '' }}">
+        <input class="form-control" type="number" placeholder="Nombre de pièce mim" name="rooms" value="{{ $input['rooms'] ?? '' }}">
+        <input class="form-control" type="number" placeholder="Budget max" name="price" value="{{ $input['price'] ?? '' }}">
+        <input class="form-control" type="text" placeholder="Mot clef" name="title" value="{{ $input['title'] ?? '' }}">
+        <button class="btn btn-primary btn-sm flex-grow-0">
+          Rechercher
+        </button>
       </form>
     </div>
     <div class="container">
       <div class="row">
-        @foreach ($properties as $property)
+        @forelse ($properties as $property)
           <div class="col-3 mb-4">
             @include('property.card')
           </div>
-        @endforeach
+        @empty 
+          <div class="text-center col mb-4">
+            Aucun biens ne correspond pas à vore rechercher
+          </div>
+        @endforelse
       </div>
     </div>
 
     <div class="container my-4">
       {{ $properties->links() }}
     </div>
+
 @endsection
