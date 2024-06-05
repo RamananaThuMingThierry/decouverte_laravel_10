5c3c36bcaa80c1290faebf231c8ee48cec133fdb
RAMANANA Thu Ming Thierry <ramananathumingthierry@gmail.com>
2024-06-05 14:00:59 +0300

    Nous avons faire le breezy

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,6 @@ class HomeController extends Controller
 {
     public function index(){
         $properties = Property::with('images')->recent()->available(true)->limit(4)->get();
-        dd($properties->first()->sold);
         return View('home', compact('properties'));
     }
 }
